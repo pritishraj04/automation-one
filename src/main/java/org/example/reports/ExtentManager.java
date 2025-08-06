87e6e3c92b491ffa31be97669b4b509c6e5b9968
Pritish Raj <pritishraj04@gmail.com>
2025-08-06 13:55:33 +0530

    report path changed

--- src/main/java/org/example/reports/ExtentManager.java
+++ src/main/java/org/example/reports/ExtentManager.java
@@ -8,7 +8,7 @@ public class ExtentManager {
 
     public static ExtentReports getInstance() {
         if (extent == null) {
-            String reportPath = System.getProperty("user.dir") + "/test-output/extent-report.html";
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReports/index.html";
             ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
             sparkReporter.config().setReportName("Automation Test Report");
             sparkReporter.config().setDocumentTitle("Test Execution Report");
